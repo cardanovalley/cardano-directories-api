73448f2d4d0d0cbf7ca8096c6c8e70d5f7935348
nicky-eng <nicolasoyarzabal@gmail.com>
2021-06-24 17:35:19 -0300

    Installed django-heroku and modifed settings

--- adastores/settings.py
+++ adastores/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -144,3 +145,5 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ]
 }
+
+django_heroku.settings(locals())
