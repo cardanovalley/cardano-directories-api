94ddfe3f531bdc5bec2072bafd2af95150545eb7
nicky-eng <nicolasoyarzabal@gmail.com>
2021-07-22 14:36:15 -0300

    Set Debug=False

--- adastores/settings.py
+++ adastores/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = ["testing-cardano-back.herokuapp.com"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
