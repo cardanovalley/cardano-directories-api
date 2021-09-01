6a6b6178b56c39c027da2357b8cc2a07e1646fe0
nicky-eng <nicolasoyarzabal@gmail.com>
2021-09-01 11:35:10 -0300

    Set page size to 9

--- adastores/settings.py
+++ adastores/settings.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 5,
+    "PAGE_SIZE": 9,
 }
 
 django_heroku.settings(locals())
