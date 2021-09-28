ed6f7466604533895fdc2af269f330af36eceb07
nicky-eng <nicolasoyarzabal@gmail.com>
2021-09-28 18:34:41 -0300

    Modified StoreSerializer. Added URL in CountrySerializer

--- stores/serializers.py
+++ stores/serializers.py
@@ -2,15 +2,13 @@ from rest_framework import serializers
 from stores.models import Store, Country
 
 
-class CountrySerializer(serializers.ModelSerializer):
+class CountrySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Country
-        fields = ["name"]
+        fields = ["name", "url"]
 
 
 class StoreSerializer(serializers.HyperlinkedModelSerializer):
-    country = CountrySerializer(required=False, read_only=True)
-
     class Meta:
         model = Store
         fields = [
@@ -21,3 +19,19 @@ class StoreSerializer(serializers.HyperlinkedModelSerializer):
             "picture",
             "country",
         ]
+
+    def to_representation(self, instance):
+        store_representation = super().to_representation(instance)
+
+        country_name = None
+        if instance.country is not None:
+            country_name = instance.country.name
+        store_representation["country_name"] = country_name
+
+        return store_representation
+
+    # def create(self, validated_data):
+    #     country_data = validated_data.pop("country")
+    #     store = Store.objects.create(**validated_data)
+    #     store.country = country_data
+    #     return store
